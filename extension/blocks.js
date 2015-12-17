41b62352e2d2d36c8250fe0366486cfaec98681f
Brooks Mershon <mershon.brooks@gmail.com>
2015-12-17 15:11:13 -0500

    handle wikibooks, wikipedia, blocks hrefs style and extraction of tags and categories

--- extension/blocks.js
+++ extension/blocks.js
@@ -1,82 +1,133 @@
-/*
-	Copyright 2015, Brooks Mershon
+/**
+ *	Copyright 2015, Brooks Mershon
+ *
+ *	Content script attempts to record information about a block on bl.ocks.org as 
+ *  relevant parts of the page are rendered.
+ */
 
-	The content script finds information on a bl.ocks.org/<username>/<gistid> and attempts
-	to insert a new gist into the database with the following properties:
-	
-	{gistid, username, description[, tags][, categories]}
-
-	N.B. [, property] means the property may or may not be present
-
-	This content script is injected into the page after the document has loaded; however, certain
-	elements are rendered after the DOM has initially loaded (e.g., .gist-readme). 
-*/
-
-var	config = {
-		childList: true,
-		subtree: true
+var	doc = document.documentElement,
+	config = {childList: true, subtree: true},
+	origins = {
+		"https://en.wikipedia.org/wiki/": "wikipedia",
+		"https://en.wikibooks.org/wiki/": "wikibooks",
+		"bl.ocks.org": "ocks"
+	},
+	tagged = {
+		"wikipedia": wikipediaTags,
+		"wikibooks": wikibooksTags
+	},
+	categorized = {
+		"wikipedia": wikipediaCategories,
+		"wikibooks": wikibooksCategories
 	};
 
+var originRE = new RegExp(Object.keys(origins).join("|"), 'gi');
+
 var observer = new MutationObserver(function(mutations) {
-	update();
+	var targets = mutations.map(function(d) {return d.target});
+	update(targets);
 });
 
-observer.observe(document.documentElement, config);
+observer.observe(doc, config);
 
 var body = d3.select('body');
 
-function update() {
-
-	var description = body.select('h1').filter('h1:not(.recorded)').classed('recorded', true);
+function update(targets) {
+	// empty selection if already recorded
+	var description = body.select('h1')
+						.filter('h1:not(.recorded)')
+						.classed('recorded', true);
 
 	var path = location.pathname.substring(1).split("/")
       	username = path[0],
      	gistid = path[1],
      	sha = path[2]; // TODO
-
-    var tags = [],
+		tags = [],
     	categories = [];
 
-	var links = body.select('.gist-readme').selectAll('a').filter('a:not(.recorded)')
+    // find links that have not yet been recorded; extract tags/categories from href
+	var links = body.select('.gist-readme').selectAll('a')
+					.filter('a:not(.recorded)')
 					.each(function(d) {
 						var href = d3.select(this).attr('href');
-						if(href.indexOf('en.wikipedia.org/wiki/') >= 0) {
-							var components = href.split("/");
-							var tokens = decodeURIComponent(components[components.length-1]).split("_");
-							tokens.forEach(function(t) {tags.push(t.toLowerCase())});
+						if(originRE.test(href)) {
+							var newTags = extractTags(href);
+							var newCategories = extractCategories(href);
+							tags = tags.concat(newTags);
+							categories = categories.concat(newCategories);
 						}
-	});
+					});
 
-	links.classed('recorded', true).classed('wiki', function(d) {
-		return d3.select(this).attr('href').indexOf('en.wikipedia.org/wiki/') >= 0
-	});
+	// classify and mark links so they are not recorded twice
+	links.attr("class", function(d) {
+		return classifyOrigin(d3.select(this).attr('href'));
+	})
+	.classed('recorded', true);
 
-	var gist = {
-		description: (description.size()) ? description.text() : "",
-		username: (description.size()) ? username : "",
-		gistid: gistid,
-		tags: tags,
-		categories: categories
+	var gist = {gistid: gistid};
+	if(description.size()) gist.description = description.text();
+	if(description.size()) gist.username = username;
+	if(tags.length) gist.tags = tags;
+	if(categories.length) gist.categories = categories;
+
+	// record in database if there is ANY new information
+	if(Object.keys(gist).length > 1) {
+		chrome.runtime.sendMessage({method:"foundGist", gist: gist}, function(result) {
+			observer.disconnect();
+			// TODO indicate successful recording of information from each rendered section
+			console.log("recorded data for gist:\n" + JSON.stringify(gist, null, 4));
+			console.log(result);
+			observer.observe(doc, config);
+		});
 	}
-
-	console.log(JSON.stringify(gist, null, 4));
-
-	chrome.runtime.sendMessage({method:"foundGist", gist: gist}, function(result) {
-		// must not observe our own mutations
-		//indicateSuccess(!result.error && result.success);
-	});
 }
 
+function classifyOrigin(href) {
+	for(s in origins) {
+		if(origins.hasOwnProperty(s)){
+			var siteRE = new RegExp(s);
+			if(siteRE.test(href)){
+				return origins[s];
+			}
+		}
+	}
+}
 
-// green checkmark if gist updated/inserted successfully, or red 'X' if there is a server error
-function indicateSuccess(success) {
-	var that = d3.select('h1');
-	var desc = that.text();
-	that.text(null);
-	that.html("<span>" + desc +"</span>");
-	that.append('span').html(((success) ? " \u2713" : " \u2717"))
-		.style("opacity", 0)
-		.style("color", (success) ? "#6AA354" : "#d60000")
-		.transition().duration(200)
-		.style("opacity", 1);
+function extractTags(href) {
+	return tagged[classifyOrigin(href)](href);
+}
+
+function extractCategories(href) {
+	return categorized[classifyOrigin(href)](href);
+}
+
+// e.g. https://en.wikipedia.org/wiki/Analemma
+function wikipediaTags(href) {
+	var components = path(href).split("/");
+	var tokens = decodeURIComponent(components[components.length-1]).toLowerCase().split("_");
+	return tokens;
+}
+
+function wikipediaCategories(href) {
+	// no categories in URL
+	return [];
+}
+
+function wikibooksTags(href) {
+	var components = path(href).split("/");
+	var tokens = decodeURIComponent(components[components.length-1]).toLowerCase().split("_");
+	return tokens;
+}
+
+// e.g. https://en.wikibooks.org/wiki/Algorithm_Implementation/Geometry/Convex_hull/Monotone_chain
+function wikibooksCategories(href) {
+	var components = path(href).split("/");
+	var strings = decodeURIComponent(components.slice(2, -1));
+	return strings;
+}
+
+function path(href) {
+	var parser = document.createElement('a');
+	parser.href = href;
+	return parser.pathname;
 }
